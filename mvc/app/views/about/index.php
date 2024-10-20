6c1d203822d8b06fc20a952cc88bcd4d379ae523
Syerli Aryanti <125714647+syrlyarynt@users.noreply.github.com>
2024-10-20 21:01:41 +0700

    Add files via upload

---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    
+    <h1 class="mt-5">About Me</h1>
+    <img src="<?= BASEURL; ?>>/img/profile.jpg" alt="Syerli Aryanti Nurafifa"
+    width="200" class="rounded-circle shadow">
+    <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
